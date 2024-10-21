b09001eb30dcd6c350f64f1ddba9c2113326fc4f
ajpark6 <149694098+ajpark6@users.noreply.github.com>
2024-10-21 18:09:13 -0500

    Update ForwardKinematics.py

--- ForwardKinematics.py
+++ ForwardKinematics.py
@@ -11,7 +11,18 @@ from scipy.linalg import expm
 from ModernRobotics import *
 
 #Creating all necessary symbols
-h, L1, L2, L3, L4, L5, L6, theta1, theta2, theta3, theta4, theta5, theta6 = symbols('h, L1, L2, L3, L4, L5, L6, theta1, theta2, theta3, theta4, theta5, theta6')
+#h, L1, L2, L3, L4, L5, L6, theta1, theta2, theta3, theta4, theta5, theta6 = symbols('h, L1, L2, L3, L4, L5, L6, theta1, theta2, theta3, theta4, theta5, theta6')
+
+h = 2
+L1, L2, L3, L4, L5, L6 = (6, 5, 4, 3, 2, 1)
+theta1, theta2, theta3, theta4, theta5, theta6 = (0,0,0,0,0,0)
+
+Mend = np.array([[1,0,0,L4+L6],[0,1,0,-(L1+L2+L3+L5)],[0,0,1,h],[0,0,0,1]])
+M2 = np.array([[1,0,0,0],[0,1,0,-(L1)],[0,0,1,h],[0,0,0,1]])
+M3 = np.array([[1,0,0,0],[0,1,0,-(L1+L2)],[0,0,1,h],[0,0,0,1]])
+M4 = np.array([[1,0,0,0],[0,1,0,-(L1+L2+L3)],[0,0,1,h],[0,0,0,1]])
+M5 = np.array([[1,0,0,L4],[0,1,0,-(L1+L2+L3)],[0,0,1,h],[0,0,0,1]])
+M6 = np.array([[1,0,0,L4],[0,1,0,-(L1+L2+L3+L5)],[0,0,1,h],[0,0,0,1]])
 
 w1 = np.array([0,0,1])
 w2 = np.array([1,0,0])
@@ -43,23 +54,31 @@ S6 = np.concatenate((w6, v6))
 
 S = np.array([S1, S2, S3, S4, S5, S6]).T
 
-Sb1 = Matrix(VecTose3(S1))
+Sb1 = VecTose3(S1)
 Sb2 = VecTose3(S2)
 Sb3 = VecTose3(S3)
 Sb4 = VecTose3(S4)
 Sb5 = VecTose3(S5)
 Sb6 = VecTose3(S6)
+#print(theta1, Sb1, expm(Sb1*theta1))
 
-P1, D1 = Sb1.diagonalize()
+e1 = expm(Sb1*theta1)
+e2 = expm(Sb2*theta2)
+e3 = expm(Sb3*theta3)
+e4 = expm(Sb4*theta4)
+e5 = expm(Sb5*theta5)
+e6 = expm(Sb6*theta6)
 
-ed1 = Matrix([exp()])
-"""
-e1 = simplify(exp(Sb1*theta1))
-e2 = simplify(exp(Sb2*theta2))
-e3 = simplify(exp(Sb3*theta3))
-e4 = simplify(exp(Sb4*theta4))
-e5 = simplify(exp(Sb5*theta5))
-e6 = simplify(exp(Sb6*theta6))
-"""
-#T01 = e1 @ e2 @ e3 @ e4 @ e5 @ e6 @ M
-#print(T01)
+T01 = e1 @ M2
+T02 = e1 @ e2 @ M3
+T03 = e1 @ e2 @ e3 @ M4
+T04 = e1 @ e2 @ e3 @ e4 @ M5
+T05 = e1 @ e2 @ e3 @ e4 @ e5 @ M6
+T06 = e1 @ e2 @ e3 @ e4 @ e5 @ e6 @ Mend
+
+print(T01)
+print(T02)
+print(T03)
+print(T04)
+print(T05)
+print(T06)
